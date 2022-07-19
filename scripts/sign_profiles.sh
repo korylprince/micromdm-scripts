f98fb591c2fc454545559387a7a697a011e32f48
xastherion <xastherion@users.noreply.github.com>
2022-07-19 18:18:30 +0200

    Added the script the hability to receive certificate password
    Hey Kory, so is possible to give the certificate password, and the result is with sign-profilename.mobileconfig ouput.
    Here you can give the pfad too, now is a bash variable.
    Regards, xastherion

--- scripts/sign_profiles.sh
+++ scripts/sign_profiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Signs all the profiles in ../profiles and puts them in OUTPUT/
+INPUT=$1
+PASSW=$2
 
-OUTPUT=$1
-
-for fn in ../profiles/*.mobileconfig; do
-    f=$(basename "$fn")
-    mdmctl apply profiles -cert ../signing_cert.p12 -sign -f "$fn" -out "$1/$f"
+for fn in $1/*.mobileconfig; do
+        f=$(basename "$fn")
+        mdmctl apply profiles -cert signing_cert.p12 -sign -f "$fn" -password $2 -out "$1/signed-$f"
 done
